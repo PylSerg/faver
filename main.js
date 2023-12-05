c05c11c5afe9091264ba80b906ba606eac9e47aa
PylSerg <pylypenko.sn@gmail.com>
2023-12-05 20:16:44 +0200

    adds actions in users cards

--- main.js
+++ main.js
@@ -165,36 +165,38 @@ function createCards() {
 		const description = `"Birthday: ${data.BIRTHDAY}"`;
 
 		const userCard = document.createElement("li");
+
 		userCard.setAttribute("class", "user");
+
 		refFavorite.append(userCard);
 
 		userCard.innerHTML = `
 					<div class="user-title">
-						<div class="user-avatar" style="background-image: url(${avatar})" onClick="openGallery(${user})"></div>
+						<div class="user-avatar" style="background-image: url(${avatar})" action="open-gallery" key=${user} ></div>
 						
 						<span class="user-name" title=${description}>${data.NAME}</span>
 					</div>
 
 					<div class="buttons">
 						<div class="buttons-profiles-block">
-							<button class="button-profiles" type="button" onClick="profiles({fbp: ${fbpId}, inst: ${instId}})">Profiles</button>
+							<button class="button-profiles" type="button" action="open-all-profiles" key=${user}>Profiles</button>
 								
 							<div class="buttons-profiles-individually">
-								<button class="button-open-instagram" type="button" onClick="Instagram_Profile(${instId})">Instagram</button>
-								<button class="button-open-facebook" type="button" onClick="Facebook_Profile(${fbpId})">Facebook</button>
+								<button class="button-open-instagram" type="button" action="open-instagram-profile" key=${user}>Instagram</button>
+								<button class="button-open-facebook" type="button" action="open-facebook-profile" key=${user}>Facebook</button>
 							</div>
 						</div>
 					
 						<div class="buttons-stories-block">
-							<button class="button-stories" type="button" onClick="stories({fbs: ${fbsId}, inst: ${instId}})">Stories</button>
+							<button class="button-stories" type="button" action="open-all-stories" key=${user}>Stories</button>
 
 							<div class="buttons-stories-individually">
-								<button class="button-open-instagram" type="button" onClick="Instagram_Stories(${instId})">Instagram</button>
-								<button class="button-open-facebook" type="button" onClick="Facebook_Stories(${fbsId})">Facebook</button>
+								<button class="button-open-instagram" type="button" action="open-instagram-stories" key=${user}>Instagram</button>
+								<button class="button-open-facebook" type="button" action="open-facebook-stories" key=${user}>Facebook</button>
 							</div>
 						</div>
 
-						<button class="button-all" type="button" onClick="openAll({fbp: ${fbpId}, fbs: ${fbsId}, inst: ${instId}})">Open All</button>
+						<button class="button-all" type="button" action="open-all-user-pages" key=${user}>Open All</button>
 					</div>
 				`;
 	}
@@ -820,6 +822,62 @@ function checkDirection() {
 refAccessInput.addEventListener("input", (e) => autoSendingPassword(e.currentTarget.value));
 
 refGuiSwitcher.addEventListener("click", () => toggleGUI());
+
+refFavorite.addEventListener("click", (e) => {
+	if (!e.target.attributes["action"]) return;
+
+	const action = e.target.attributes["action"].value;
+	const userId = e.target.attributes.key.value;
+	const user = favorite[userId];
+
+	if (action === "open-gallery") {
+		openGallery(userId);
+
+		return;
+	}
+
+	if (action === "open-all-profiles") {
+		profiles({ fbp: user.FB_PROF_ID, inst: user.INST_ID });
+
+		return;
+	}
+
+	if (action === "open-instagram-profile") {
+		Instagram_Profile(user.INST_ID);
+
+		return;
+	}
+
+	if (action === "open-facebook-profile") {
+		Facebook_Profile(user.FB_PROF_ID);
+
+		return;
+	}
+
+	if (action === "open-all-stories") {
+		stories({ fbs: user.FB_STOR_ID, inst: user.INST_ID });
+
+		return;
+	}
+
+	if (action === "open-instagram-stories") {
+		Instagram_Stories(user.INST_ID);
+
+		return;
+	}
+
+	if (action === "open-facebook-stories") {
+		Facebook_Stories(user.FB_STOR_ID);
+
+		return;
+	}
+
+	if (action === "open-all-user-pages") {
+		openAll({ fbp: user.FB_PROF_ID, fbs: user.FB_STOR_ID, inst: user.INST_ID });
+
+		return;
+	}
+});
 
 refGallery.addEventListener("touchstart", (e) => {
 	touchstartX = e.changedTouches[0].screenX;
